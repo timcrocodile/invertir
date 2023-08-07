de38339a44e9e29b6c5ea8d3bd8552673307538a
timcrocodile <timcrocodile@proton.me>
2023-08-07 11:01:37 +0200

    token

--- src/mocks/info.ts
+++ src/mocks/info.ts
@@ -214,7 +214,7 @@ export const info: infoType[] = [
   },
   {
     id: 3,
-    title: "Arbitrato per le controversie finanziarie",
+    title: "Arbitrato nuovo per le controversie finanziarie",
     content:
       "Il Cliente può presentare ricorso all’Arbitro per le Controversie Finanziarie (ACF), istituito presso la CONSOB, per la risoluzione extragiudiziale delle controversie tra investitori (diversi dai clienti professionali) e i consulenti finanziari autonomi e le società di consulenza finanziaria di cui, rispettivamente, agli articoli 18-bis e 18-ter del TUF, , relative alla violazione da parte di questi ultimi degli obblighi di diligenza, correttezza, informazione e trasparenza nell’esercizio dell’attività disciplinata dalla Parte II del TUF, incluse le controversie oggetto del Regolamento (UE) n. 524/2013. Non rientrano nell’ambito dell’operatività dell’Arbitro le controversie che implicano la richiesta di somme di denaro per un importo superiore a Euro cinquecentomila. Sono esclusi dalla cognizione dell’Arbitro i danni che non sono conseguenza immediata e diretta dell’inadempimento o della violazione da parte dell’intermediario degli obblighi sopra descritti e quelli che non hanno natura patrimoniale. Il diritto di ricorrere all’ACF è irrinunciabile e sempre esercitabile anche in presenza di clausole di devoluzione delle controversie ad altri organismi di risoluzione extragiudiziale contenute nel contratto. Per maggiori informazioni fare riferimento al sito internet https://www.acf.consob.it/.",
     subContent: null,
